5fd9b8735c377fb8c09b77c9cfc9cf8f179c187b
Charbel Hleyhel <CharbelHleyhel@outlook.com>
2023-11-20 23:58:47 +0200

    Add parallax effect to header

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', function() {
     var header = document.getElementsByClassName('header');
-    if (window.pageYOffset > 0) {
+    if (window.scrollY > 0) {
       header.classList.add('scroll');
     } else {
       header.classList.remove('scroll');
     }
+  });
+
+  window.addEventListener('scroll', function() {
+    var parallax = document.getElementById('parallax');
+    var scrolled = window.scrollY;
+    parallax.style.transform = 'translate3d(0, ' + scrolled * 0.5 + 'px, 0)';
   });
