7b373b405d2a287fd8abde47f1197d5a189ecaaa
Charbel Hleyhel <CharbelHleyhel@outlook.com>
2023-11-24 00:10:39 +0200

    chatgpt

--- menu.js
+++ menu.js
@@ -12,3 +12,37 @@ window.addEventListener('scroll', function() {
     var scrolled = window.scrollY;
     parallax.style.transform = 'translate3d(0, ' + scrolled * 0.5 + 'px, 0)';
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Get all the section elements
+    const sections = document.querySelectorAll(".section");
+  
+    // Add a wheel event listener to handle scrolling
+    document.addEventListener("wheel", function(event) {
+      // Calculate the direction of the scroll
+      const direction = event.deltaY > 0 ? 1 : -1;
+  
+      // Find the current active section
+      let currentSection;
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 0 && rect.bottom > 0) {
+          currentSection = section;
+        }
+      });
+  
+      // Find the next section based on the direction
+      let nextSection;
+      if (currentSection) {
+        const currentIndex = Array.from(sections).indexOf(currentSection);
+        nextSection = sections[currentIndex + direction];
+      }
+  
+      // Scroll to the next section if available
+      if (nextSection) {
+        nextSection.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+  
+  
